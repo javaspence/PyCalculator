93e4c6095a305a8ce269e94393b5ccbf935cacac
javaspence <54484567+javaspence@users.noreply.github.com>
2021-03-03 14:13:21 -0600

    Added square root calculating; updated to Python 3

--- py/main.py
+++ py/main.py
@@ -1,20 +1,21 @@
-#the only reason this is python 2.7 is because they didn't add support for str(input()) in python 3
 print('PyCalculator v0.2-alpha (Mark I)')
 print('Developed by Spencer Roudebush')
 import pycalc
 from pycalc import number1, number2, method
+import math
+
 
 if method == str('add'):
   print(number1 + number2)
-  reload(pycalc)
 
 if method == str('subtract'):
   print(number1 - number2)
-  reload(pycalc)
 
 if method == str('multiply'):
   print(number1 * number2)
-  reload(pycalc)
 
 if method == str('divide'):
   print(number1 / number2)
+
+if method == str('square root'):
+  print(math.sqrt(number1))
