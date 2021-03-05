4522b99fd1c6e62770c54b85dc44a5942b06f5bb
javaspence <54484567+javaspence@users.noreply.github.com>
2021-03-05 08:10:06 -0600

    Update main.py

--- py/main.py
+++ py/main.py
@@ -1,21 +1,4 @@
 print('PyCalculator v0.2-alpha (Mark I)')
 print('Developed by Spencer Roudebush')
-import pycalc
-from pycalc import number1, number2, method
-import math
-
-
-if method == str('add'):
-  print(number1 + number2)
-
-if method == str('subtract'):
-  print(number1 - number2)
-
-if method == str('multiply'):
-  print(number1 * number2)
-
-if method == str('divide'):
-  print(number1 / number2)
-
-if method == str('square root'):
-  print(math.sqrt(number1))
+import os
+os.system('python pycalc-main.py')
