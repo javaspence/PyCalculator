fd8cd3a66b2c95450a6d2efc3257f14705d501cc
javaspence <54484567+javaspence@users.noreply.github.com>
2021-03-05 12:33:15 -0600

    Create makeexecutable.sh

---
+++ makefile/makeexecutable.sh
@@ -0,0 +1,3 @@
+#if you really want to use this, change the path accordingly because this was my own path that I used.
+cd C:\Users\javaspence\Downloads\pycalculator\main.py
+pyinstaller --onefile main.py
