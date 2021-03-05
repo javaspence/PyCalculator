8c8eb9b05fa311a6cfb0bf09016b82432c91da2c
javaspence <54484567+javaspence@users.noreply.github.com>
2021-03-05 12:41:10 -0600

    Create execconvert.sh

---
+++ execconvert.sh
@@ -0,0 +1,4 @@
+pip install pyinstaller
+#if you really want to use this, change the path accordingly because this was my own path that I used.
+cd C:\Users\javaspence\Downloads\pycalculator\main.py
+pyinstaller --onefile main.py
